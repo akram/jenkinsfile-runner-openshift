dbfbf73d6e42ca466e267797cf31449f27a6158f
Akram Ben Aissi <akram.benaissi@gmail.com>
2021-06-13 23:08:01 +0200

    Use run syntax command and default to /workspace directory

--- jenkinsfile-runner-launcher.sh
+++ jenkinsfile-runner-launcher.sh
@@ -5,7 +5,7 @@ export JAVA_OPTS="${JAVA_OPTS} -Djenkins.model.Jenkins.slaveAgentPort=50000"
 export JAVA_OPTS="${JAVA_OPTS} -Djenkins.model.Jenkins.slaveAgentPortEnforce=true"
 export JAVA_OPTS="${JAVA_OPTS} -Dhudson.slaves.NodeProvisioner.initialDelay=1" # How long to wait after startup before starting to provision nodes from clouds
 export JAVA_OPTS="${JAVA_OPTS} -Dhudson.slaves.ConnectionActivityMonitor.timeToPing=30000" # wait after startup to start checking agent connections, in milliseconds.
-
-/app/bin/jenkinsfile-runner-launcher -f /workspace/jenkinsfile
+cd /workspace
+/app/bin/jenkinsfile-runner-launcher run
 
 
